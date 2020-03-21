501ea98401eab8d27fb0c57eea81344d5080435d
Fabio Xavier <fabio.xavier@oobj.com.br>
2020-03-21 17:46:28 -0300

    Atualizando template

--- front-end/src/app/profile/profile.component.ts
+++ front-end/src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-profile',
@@ -8,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
 
 export class ProfileComponent implements OnInit {
 
-    constructor() { }
+    _id: string;
+
+    constructor(private route: ActivatedRoute) {
+        this.route.params.subscribe(params => this._id = params['id']);
+        console.log(this._id)
+     }
 
     ngOnInit() {}
 
