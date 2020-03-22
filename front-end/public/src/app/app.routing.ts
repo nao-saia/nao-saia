cc436737f61637b7c3dde1491818e55485e861bd
Fabio Xavier <fabio.xavier@discente.ufg.br>
2020-03-22 14:29:22 -0300

    Adicionando dados

--- front-end/public/src/app/app.routing.ts
+++ front-end/public/src/app/app.routing.ts
@@ -15,7 +15,7 @@ const routes: Routes =[
     { path: 'register',           component: SignupComponent },
     { path: 'landing',          component: LandingComponent },
     { path: 'login',          component: LoginComponent },
-    { path: '', redirectTo: 'home', pathMatch: 'full' }
+    { path: '', redirectTo: 'landing', pathMatch: 'full' }
 ];
 
 @NgModule({
