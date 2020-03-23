243eb6c0bf412a8c3198d5b30966f37b50ae0830
Taynan Rezende <taynan.rezende@oobj.com.br>
2020-03-23 00:02:46 -0300

    Alterando List para Set

--- back-end-2/nao-saia/src/main/java/br/com/nao/saia/repository/MerchantRepository.java
+++ back-end-2/nao-saia/src/main/java/br/com/nao/saia/repository/MerchantRepository.java
@@ -4,20 +4,23 @@ import br.com.nao.saia.model.Merchant;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.geo.Distance;
 import org.springframework.data.geo.Point;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @Repository
 public interface MerchantRepository extends MongoRepository<Merchant, UUID> {
 	
-	List<Merchant> findByCategoriesIn(String category, Pageable pageable);
+	Set<Merchant> findByCategoriesIn(String category, Pageable pageable);
 	
-	List<Merchant> findByAddressCity(String city, Pageable pageable);
+	Set<Merchant> findByAddressCity(String city, Pageable pageable);
 	
-	List<Merchant> findByAddressState(String city, Pageable pageable);
+	Set<Merchant> findByAddressState(String city, Pageable pageable);
 	
-	List<Merchant> findByAddressLocationNear(Point point, Distance distance, Pageable pageable);
+	Set<Merchant> findByAddressLocationNear(GeoJsonPoint geoJsonPoint, Pageable pageable);
+	
+	Set<Merchant> findByAddressLocationNear(Point point, Distance distance, Pageable pageable);
 	
 }
