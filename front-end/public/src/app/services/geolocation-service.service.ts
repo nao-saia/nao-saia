e1b5769bfa89b63d0ffee4dc036d3cc6b67b0a3e
Alexandre Teixeira <alexanddy99@gmail.com>
2020-03-25 19:27:17 -0300

    merge

--- front-end/public/src/app/services/geolocation-service.service.ts
+++ front-end/public/src/app/services/geolocation-service.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subscriber, zip } from 'rxjs';
 import { Address } from '../domain/Address';
 import { HttpWrapperService } from './http-wrapper.service';
 
@@ -11,26 +11,26 @@ export class GeolocationService {
   constructor(private http: HttpWrapperService) { }
 
   getCurrentLocation(address: Address): Observable<Address> {
-    return new Observable((observer) => {
+    return new Observable((subscriber) => {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position: Position) => {
           if (position) {
             this.getAddressFromLocation(position.coords.latitude, position.coords.longitude)
-                .subscribe((responseAddress: Address) => observer.next(responseAddress));
+              .subscribe((responseAddress: Address) => subscriber.next(responseAddress));
           } else {
-            this.getAddressFromZipCode(address.zipcode)
-                .subscribe((responseAddress: Address) => observer.next(responseAddress));
+            this._handlePositionError(address.zipcode, subscriber);
           }
-        }, (error) => {
-          console.log(error)
-          this.getAddressFromZipCode(address.zipcode)
-              .subscribe((responseAddress: Address) => observer.next(responseAddress));
-        });
+        }, () => this._handlePositionError(address.zipcode, subscriber));
       } else {
-        this.getAddressFromZipCode(address.zipcode)
-            .subscribe((responseAddress: Address) => observer.next(responseAddress));
+        this._handlePositionError(address.zipcode, subscriber);
       }
     });
+  }
+
+  _handlePositionError(zipCode: string, subscriber: Subscriber<Address>): void {
+    this.getAddressFromZipCode(zipCode)
+      .subscribe((responseAddress: Address) => subscriber.next(responseAddress)
+      , (error) => subscriber.error(error));
   }
 
   getAddressFromZipCode(zipCode: string): Observable<Address> {
