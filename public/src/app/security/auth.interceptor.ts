9a2bc988120a389f710c7bd783e3cf05eec28d24
Alexandre Teixeira <alexanddy99@gmail.com>
2020-04-04 13:14:30 -0300

    Melhorado o interceptor de auth Adicionado redirect to login, quando o response status for 401

--- public/src/app/security/auth.interceptor.ts
+++ public/src/app/security/auth.interceptor.ts
@@ -4,21 +4,32 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
 import { Observable, combineLatest, Observer } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private injector: Injector) { }
+  constructor(private injector: Injector, private router: Router) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<any>> {
     const userService = this.injector.get(UserService);
     const token = userService.getToken();
     if (token) {
       const authRequest = request.clone({ setHeaders: { 'Authorization': `Bearer ${token}` } });
-      return next.handle(authRequest);
+      return next.handle(authRequest).pipe(tap(() => { },
+        (err: any) => {
+          if (err instanceof HttpErrorResponse) {
+            if (err.status !== 401) {
+              return;
+            }
+            this.router.navigate(['/login']);
+          }
+        }));
     }
     return next.handle(request);
   }
